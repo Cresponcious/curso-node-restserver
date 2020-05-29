499ced4a982b43bfa82e61add5b5a8faa2eeebb6
Cresponcious <agatica77@gmail.com>
2020-05-29 17:33:42 -0400

    Ajuste a variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://Crespo:pdfmgxat23YK3LrG@cluster0-cvrxb.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
