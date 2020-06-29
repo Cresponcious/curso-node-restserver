99857300f6c0a763367167b27ea9baa9ea822af5
Cresponcious <agatica77@gmail.com>
2020-06-29 00:43:46 -0400

    google sign in

--- server/config/config.js
+++ server/config/config.js
@@ -25,12 +25,15 @@ process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
 //Base de datos
 // =====================================
 let urlDB;
-
-
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     urlDB = process.env.MONGO_URI;
 }
-
 process.env.URLDB = urlDB;
+
+
+// =====================================
+// Google client
+// =====================================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '614942013317-8upmca6j0eg30qdj9hdfmoino5vn01ql.apps.googleusercontent.com'
--- server/server.js
+++ server/server.js
@@ -3,12 +3,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+
+//Habilitar uso de carpeta publica
+app.use(express.static(path.resolve(__dirname, '../public')));
+
 
 app.use(require('./routes/index'));
 
